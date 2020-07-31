596dfa44523e5a7875666a9069bf587577db7d89
Aviv Eyal <avivey@gmail.com>
2020-07-31 05:05:43 -0700

    Remove lints when closing editor

--- src/extension.ts
+++ src/extension.ts
@@ -2,39 +2,40 @@ import * as vscode from 'vscode';
 import * as lint from './arc_lint';
 
 export function activate(context: vscode.ExtensionContext) {
-	const collection = vscode.languages.createDiagnosticCollection('arc lint');
+	const diagnostics = vscode.languages.createDiagnosticCollection('arc lint');
 
 	lint.setup();
 
 	function d(disposable: vscode.Disposable) {
 		context.subscriptions.push(disposable);
 	}
+	d(diagnostics);
 
-	d(vscode.commands.registerCommand('arc-vscode.clearLint', () => collection.clear()));
+	d(vscode.commands.registerCommand('arc-vscode.clearLint', () => diagnostics.clear()));
 
 	d(vscode.workspace.onDidSaveTextDocument(onTextDocumentEvent));
 	d(vscode.workspace.onDidOpenTextDocument(onTextDocumentEvent));
 	d(vscode.workspace.onDidChangeConfiguration(onChangeConfig));
 
 	if (vscode.window.activeTextEditor) {
-		lint.lintFile(vscode.window.activeTextEditor.document, collection);
+		lint.lintFile(vscode.window.activeTextEditor.document, diagnostics);
 	}
 
 	d(vscode.window.onDidChangeActiveTextEditor(editor => {
 		if (editor) {
-			lint.lintFile(editor.document, collection);
+			lint.lintFile(editor.document, diagnostics);
 		}
 	}));
 
+	d(vscode.workspace.onDidCloseTextDocument(document => diagnostics.delete(document.uri)));
+
 	function onTextDocumentEvent(document: vscode.TextDocument) {
-		lint.lintFile(document, collection);
+		lint.lintFile(document, diagnostics);
 	}
 
 }
 
-export function deactivate() {
-	// TODO collection.clear();
-}
+export function deactivate() { }
 
 function onChangeConfig(e: vscode.ConfigurationChangeEvent) {
 	if (!e.affectsConfiguration('arc-vscode.lint')) {
